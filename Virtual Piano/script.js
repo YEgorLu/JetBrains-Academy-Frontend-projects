0b52c5e76628f6a8fd65275bd4f50e24f81d10d9
YEgorLu <99907142+YEgorLu@users.noreply.github.com>
2022-08-06 19:55:45 +0500

    Finished

---
+++ Virtual Piano/script.js
@@ -0,0 +1,11 @@
+const pianoButtons = ["KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyW", "KeyE", "KeyT", "KeyY", "KeyU"];
+
+document.onkeydown = ev => {
+  if(!pianoButtons.includes(ev.code)){
+    console.warn("Unknown key");
+    return;
+  }
+  let audio = document.createElement("audio");
+  audio.setAttribute("src", `audio/${ev.key.toUpperCase()}.mp3`);
+  audio.play();
+}
