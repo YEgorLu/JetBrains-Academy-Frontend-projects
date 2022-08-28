55c20988d60c42efa910579cc224be40367bc7de
YEgorLu <99907142+YEgorLu@users.noreply.github.com>
2022-08-28 19:04:25 +0500

    Update script.js

--- Virtual_Piano/script.js
+++ Virtual_Piano/script.js
@@ -1,13 +1,10 @@
 const pianoButtons = ["KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyW", "KeyE", "KeyT", "KeyY", "KeyU"];
-let kbds = document.querySelectorAll("kbd");
-kbds = kbds.map(el => [el.textContent, el]);
 
 document.onkeydown = ev => {
   if(!pianoButtons.includes(ev.code)){
     console.warn("Unknown key");
     return;
   }
-  kbd
 
   let audio = document.createElement("audio");
   audio.setAttribute("src", `audio/${ev.key.toUpperCase()}.mp3`);
